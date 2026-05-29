8da552e5bc9cea41ddb8bb4b7b32678090fba86a
Tsurya-azha <pandesurya120@gmail.com>
2026-05-29 23:17:54 +0800

    nambah dikit kalo berhasil

--- controllers/reviews.controller.js
+++ controllers/reviews.controller.js
@@ -51,6 +51,16 @@ export const getReviewsByBookId = async (req, res) => {
         const review = await prisma.review.findMany({
             where: {bookId: id},
             include: {reviewer: true},
+            select: {
+                id: true,
+                rating: true,
+                comment: true,
+                user: {
+                    select: {
+                        name: true
+                    }
+                }
+            }
         })
 
         res.json({
